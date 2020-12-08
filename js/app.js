ff2af6063fbb6632840c1620ceee253eeaabe536
Jordan Walker <jordanswalker28@gmail.com>
2020-12-08 00:15:49 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const url = 'https://script.google.com/macros/s/AKfycbxFlQXkvna9jYamLWhTQ1k3cJItWZDvCYoCmLTcN-CJcr7vZw/exec';
+const val = {page:1};
+const page = {};
+page.container = document.createElement('div');
+page.container.textContent = "hello world";
+page.main = document.querySelector('section');
+page.main.append(page.container);
+getCourses();
+ 
+function renderPost(data){
+  data.forEach(function(post){
+    const div = document.createElement('div');
+    div.innerHTML = `${post[8]}`;
+    page.container.append(div);
+  })
+}
+ 
+function getCourses(){
+  const baseURL = url + '?p=' + val.page;
+  fetch(baseURL).then((rep)=> rep.json())
+  .then((json) => {
+    console.log(json.data);
+    renderPost(json.data.posts)
+  })
+}
